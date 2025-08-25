f67381bbfd15ab4d9813294e1b389a2f2bec6fd4
Cadoudal <oteillet@gmail.com>
2025-08-25 12:04:05 +0200

    Add Menus Add urls.txt to gitignore Add option to download videos

--- source/downloader.py
+++ source/downloader.py
@@ -1,20 +1,42 @@
-from pytubefix import YouTube
 from pathlib import Path
+
+from pytubefix import YouTube
 from tqdm import tqdm
+
 
 def download_audio(urls: list[str], target_dir: Path, log_file: Path):
     """Télécharge l'audio de chaque URL dans target_dir avec barre de progression"""
+
     for url in tqdm(urls, desc="Téléchargement", unit="vidéo"):
         try:
             yt = YouTube(url)
             stream = yt.streams.get_audio_only()
-            
+
             if stream is None:
                 print(f"⚠️ Aucun flux audio trouvé pour {yt.title}")
                 continue  # passe à l'URL suivante
-            
+
             stream.download(output_path=str(target_dir))
-        
+
+        except Exception as e:
+            with open(log_file, "a") as log:
+                log.write(f"DOWNLOAD FAIL: {url} : {e}\n")
+
+
+def download_video(urls: list[str], target_dir: Path, log_file: Path):
+    """Télécharge la vidéo de chaque URL dans target_dir avec barre de progression"""
+
+    for url in tqdm(urls, desc="Téléchargement", unit="vidéo"):
+        try:
+            yt = YouTube(url)
+            stream = yt.streams.get_highest_resolution()
+
+            if stream is None:
+                print(f"⚠️ Aucun flux vidéo trouvé pour {yt.title}")
+                continue  # passe à l'URL suivante
+
+            stream.download(output_path=str(target_dir))
+
         except Exception as e:
             with open(log_file, "a") as log:
                 log.write(f"DOWNLOAD FAIL: {url} : {e}\n")
--- source/main.py
+++ source/main.py
@@ -1,10 +1,12 @@
-from config import URL_FILE, DOWNLOAD_DIR, LOG_FILE
-from utils import read_urls, ensure_dir
-from downloader import download_audio
+from config import DOWNLOAD_DIR, LOG_FILE, URL_FILE
 from converter import convert_m4a_to_mp3
+from downloader import download_audio, download_video
+from utils import ensure_dir, read_urls
+
 
 def main():
-    print("YT-Downloader démarré...")
+
+    print("YT-Downloader démarré")
 
     # Création du dossier de téléchargement
     ensure_dir(DOWNLOAD_DIR)
@@ -13,16 +15,35 @@ def main():
     try:
         urls = read_urls(URL_FILE)
         if urls:
-            download_audio(urls, DOWNLOAD_DIR, LOG_FILE)
+            while True:
+                print("Telecharger :")
+                print("   1. La vidéo")
+                print("   2. L'audio")
+                print("   3. Convertir uniquement")
+                choix = input("Choix : ")
+                if choix == "1":
+                    download_video(urls, DOWNLOAD_DIR, LOG_FILE)
+                    break
+                elif choix == "2":
+                    download_audio(urls, DOWNLOAD_DIR, LOG_FILE)
+                    break
+                elif choix == "3":
+                    break
+                else:
+                    print("Option invalide")
+
         else:
             print("Aucune URL trouvée dans urls.txt")
     except Exception as e:
         print(f"Erreur lecture URLs : {e}")
 
     # Conversion de m4a vers mp3 (toujours exécutée)
-    convert_m4a_to_mp3(DOWNLOAD_DIR, LOG_FILE)
+    choix = input('Voulez-vous convertir les audios en mp3 ? (o/n) : ')
+    if choix == "o":
+        convert_m4a_to_mp3(DOWNLOAD_DIR, LOG_FILE)
 
-    print("Téléchargement terminé !")
+    print("Travail terminé !")
+
 
 if __name__ == "__main__":
     main()
